f8ff833fd7388ebc069786d4141a63f61a98fdff
jy49 <jy49@njit.edu>
2014-04-23 14:40:07 -0400

    Test: machinegun goes click

--- mpgame/weapon/WeaponMachinegun.cpp
+++ mpgame/weapon/WeaponMachinegun.cpp
@@ -226,13 +226,27 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 	};	
 	switch ( parms.stage ) {
 		case STAGE_INIT:
-			if ( wsfl.zoom ) {
-				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( true, 1, spreadZoom, 0, 1.0f );
+			if (wsfl.zoom) {
+				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier(PMOD_FIRERATE));
+				if (false)
+				{
+					Attack(true, 1, spreadZoom, 0, 1.0f);
+				}
+				else
+				{
+					gameLocal.Printf("Click \n");
+				}
 				fireHeld = true;
 			} else {
 				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( false, 1, spread, 0, 1.0f );
+				if (false)
+				{
+					Attack ( false, 1, spread, 0, 1.0f );
+				}
+				else
+				{
+					gameLocal.Printf("Click \n");
+				}
 			}
 			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );	
 			return SRESULT_STAGE ( STAGE_WAIT );
